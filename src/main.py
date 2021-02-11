7171d843579600863b16b8aff01d369dcae1f626
fantasmo <gheadley27@gmail.com>
2021-02-11 13:59:17 +0000

    -source code dir and main file

---
+++ src/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+File: main.py
+Author: Greg, Leon & Jonny
+Github: https://github.com/headcase
+Description: main file
+"""
+
+if __name__ == '__main__':
+    main()
+
+
